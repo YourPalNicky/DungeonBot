301bf9acedef24db7dee4c1b7a33710a751a62c6
YourPalNicky <yourpalnicky@gmail.com>
2019-10-14 01:07:44 -0400

    Update activity.js

--- commands/activity.js
+++ commands/activity.js
@@ -12,6 +12,6 @@ exports.run = (client, message, args) => {
         data["activity"] = writeActivity;
         fs.writeFileSync("./variables.json", JSON.stringify(data, null, 2));
         statusRefresh(client);
-        return message.channel.send(createEmbed('**Successifully Changed Activity**', 'The bots activity was updated.', greenColor, message.author));
+        return message.channel.send(createEmbed('**Successfully Changed Activity**', 'The bots activity was updated.', greenColor, message.author));
     } else return message.channel.send(createEmbed('**Error**', 'No Access', redc));
 };
